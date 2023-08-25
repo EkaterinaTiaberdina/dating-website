fba686e6c985c3c80bd2b3753fc768c7bda6c972
EkterinaTiaberdina <tiaberdina749@gmail.com>
2023-08-25 16:56:02 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        id: 1,
+        name: "Джейн Остин",
+        description: "Дарить любовь гораздо важнее, чем ее получать.",
+        age: 35,
+        image: "https://images.unsplash.com/photo-1554727242-741c14fa561c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+    },
+    {
+        id: 2,
+        name: "Стивен Спирс",
+        description: "Женщина священна; женщина, которую любишь, священна вдвойне.",
+        age: 47,
+        image: "https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+    },
+    {
+        id: 3,
+        name: "Кристина Смит",
+        description: "Без любви жить легче. …Но без нее нет смысла.",
+        age: 25,
+        image: "https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80"
+    },
+    {
+        id: 4,
+        name: "Майк Спектор",
+        description: "Я пишу не для того, чтобы просить тебя прийти, я пишу, чтобы предупредить: я всегда буду ждать.",
+        age: 28,
+        image: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+    },
+    {
+        id: 5,
+        name: "Стефани Майер",
+        description: "Двое неидеальных встретили друг друга… Полюбили… И стали идеальными друг для друга…",
+        age: 24,
+        image: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80"
+    },
+]
